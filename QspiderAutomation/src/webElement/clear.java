094ab14a6c575a4a2c6ca2d20485a09b59849ae5
decent-rajat <rajatprasad2912@gmail.com>
2024-11-16 14:08:34 +0530

    clone it from local repository

--- QspiderAutomation/src/webElement/clear.java
+++ QspiderAutomation/src/webElement/clear.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class clear {
-
+// clone it from local repository
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://demo.vtiger.com/vtigercrm/");
